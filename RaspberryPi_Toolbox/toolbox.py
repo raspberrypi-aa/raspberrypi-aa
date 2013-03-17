99b010f3c9c1fcf8b51f164a2e21ddc16b9d1766
Eric Friedrich <eric.friedrich84@gmail.com>
2013-03-17 16:26:37 -0400

    Modified toolbox.py

--- RaspberryPi_Toolbox/toolbox.py
+++ RaspberryPi_Toolbox/toolbox.py
@@ -3,17 +3,37 @@
 #
 #
 
+class UnknownRevision(Exception):
+    pass
+
 class Toolbox:
     '''Set of utilities to exercise the power of the Raspberry Pi'''
     def __init__(self):
         pass
     
     def get_revision(self):
+        'Return Model and Revision of the current board'
         with open('/proc/cpuinfo', 'r') as f:
             for line in f:
                 if 'Revision' in line:
                     rev = line.split(':')[1].strip()
-                    print rev
+                    #print rev
+                    if rev == '0002':
+                        return 'Model B'
+                    elif rev == '0003':
+                        return 'Model B'
+                    elif rev == '0004':
+                        pass
+                    elif rev == '0005':
+                    elif rev == '0006':
+                    elif rev == '0007':
+                    elif rev == '0008':
+                    elif rev == '0009':
+                    elif rev == '000d':
+                    elif rev == '000e':
+                    elif rev == '000f':
+                    else:
+                        raise UnknownRevision('Revision is: ' + rev)
                 
 
 
