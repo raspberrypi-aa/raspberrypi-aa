ddc26e5b1df8a0519ba4ce933e88f9d3477c6671
Eric Friedrich <eric.friedrich84@gmail.com>
2013-03-17 22:28:24 -0400

    Modified toolbox.py

--- RaspberryPi_Toolbox/toolbox.py
+++ RaspberryPi_Toolbox/toolbox.py
@@ -35,7 +35,7 @@ class Toolbox:
  
     def gpio_setup_output(self, pin, output):
         with open("/sys/class/gpio/export", 'w') as f:
-            f.write(pin)
+            f.write(str(pin))
         with open("/sys/class/gpio%d/direction" % pin, 'w') as f:
             f.write(output)
     
