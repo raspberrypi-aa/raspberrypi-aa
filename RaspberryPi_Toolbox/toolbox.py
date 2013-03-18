43d26c95802543b37832791623224bfd48e0f7da
Eric Friedrich <eric.friedrich84@gmail.com>
2013-03-17 21:14:24 -0400

    Modified toolbox.py

--- RaspberryPi_Toolbox/toolbox.py
+++ RaspberryPi_Toolbox/toolbox.py
@@ -18,20 +18,14 @@ class Toolbox:
                 if 'Revision' in line:
                     rev = line.split(':')[1].strip()
                     #print rev
-                    if rev == '0002':
-                        return 'Model B'
-                    elif rev == '0003':
-                        return 'Model B'
-                    elif rev == '0004':
-                        pass
-                    elif rev == '0005':
-                    elif rev == '0006':
-                    elif rev == '0007':
-                    elif rev == '0008':
-                    elif rev == '0009':
-                    elif rev == '000d':
-                    elif rev == '000e':
-                    elif rev == '000f':
+                    if rev == '0002' or rev == '0003':
+                        return 'Model B, Revision 1.0'
+                    elif rev == '0004' or rev == '0005' or rev == '0006':
+                        return 'Model B, Revision 2.0'
+                    elif rev == '0007' or rev == '0008' or rev == '0009':
+                        return 'Model A'
+                    elif rev == '000d' or rev == '000e' or rev == '000f':
+                        return 'Model B, Revision 2.0'
                     else:
                         raise UnknownRevision('Revision is: ' + rev)
                 
