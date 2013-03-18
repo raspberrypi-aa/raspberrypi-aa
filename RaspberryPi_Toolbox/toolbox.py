af420a625553653affda1d9fbd916091bec49b21
Eric Friedrich <eric.friedrich84@gmail.com>
2013-03-17 21:16:06 -0400

    Modified toolbox.py

--- RaspberryPi_Toolbox/toolbox.py
+++ RaspberryPi_Toolbox/toolbox.py
@@ -19,13 +19,13 @@ class Toolbox:
                     rev = line.split(':')[1].strip()
                     #print rev
                     if rev == '0002' or rev == '0003':
-                        return 'Model B, Revision 1.0'
+                        return ('Model B', 'Revision 1.0', '256MB')
                     elif rev == '0004' or rev == '0005' or rev == '0006':
-                        return 'Model B, Revision 2.0 (256MB)'
+                        return ('Model B', 'Revision 2.0', '256MB')
                     elif rev == '0007' or rev == '0008' or rev == '0009':
-                        return 'Model A'
+                        return ('Model A', 'Revision 1.0', '256MB')
                     elif rev == '000d' or rev == '000e' or rev == '000f':
-                        return 'Model B, Revision 2.0 (512MB)'
+                        return ('Model B', 'Revision 2.0' '512MB')
                     else:
                         raise UnknownRevision('Revision is: ' + rev)
                 
@@ -33,4 +33,4 @@ class Toolbox:
 
 if __name__ == '__main__':
     t = Toolbox()
-    print t.get_revision()
+    print ', '.join(t.get_revision())
