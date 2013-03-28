da4f4f58565d102c8c3bf26b7593bd5d5d3168a4
Eric Friedrich <eric.friedrich84@gmail.com>
2013-03-28 00:13:54 +0000

    Modified gpio_test.py

--- RaspberryPi_Toolbox/gpio_test.py
+++ RaspberryPi_Toolbox/gpio_test.py
@@ -1,18 +1,8 @@
 import RPi.GPIO as GPIO
 import time
     
-if 1:
-    input_pin = 4 
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(input_pin, GPIO.IN)  
-    while True:
-        input_val = GPIO.input(input_pin)
-        print "value: "+str(input_val)
-        time.sleep(.5)
-        
-    
 # Blink an LED
-if 1:
+if 0:
     pin = 18
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(pin, GPIO.OUT)
@@ -24,7 +14,29 @@ if 1:
         GPIO.output(pin, GPIO.LOW)
         time.sleep(.5)
     
-  
+
+# Poll for input
+if 0:
+    input_pin = 4 
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(input_pin, GPIO.IN)  
+    while True:
+        input_val = GPIO.input(input_pin)
+        print "value: "+str(input_val)
+        time.sleep(.5)
+        
+if 1:
+    input_pin = 4 
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(input_pin, GPIO.IN) 
+    GPIO.add_event_detect(input_pin, GPIO.FALLING)
+    while True:
+        if GPIO.event_detected(input_pin):
+            print True
+        else
+            print False
+        time.sleep(.5)
+    
     
     
 
