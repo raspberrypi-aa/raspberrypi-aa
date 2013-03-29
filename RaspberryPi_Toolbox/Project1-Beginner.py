e3a099d5d8736e3caf7002b93112380b9b3c27da
Eric Friedrich <eric.friedrich84@gmail.com>
2013-03-29 02:46:43 +0000

    Modified Project1-Beginner.py

---
+++ RaspberryPi_Toolbox/Project1-Beginner.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# Artisan's Asylum - Introduction to Raspberry Pi
+# Project 1 - Beginner
+#    a) Timed LED Blink
+#    b) Stopwatch
+#
+# LED on Pin 18
+# Switch w/ pull resistor on pin 4
+
+
+import RPi.GPIO as GPIO
+
+
+def led_blink(period):
+    pass    
+    
+    
+GPIO.setup
+    
