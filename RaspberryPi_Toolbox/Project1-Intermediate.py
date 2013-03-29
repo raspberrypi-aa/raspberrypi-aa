6c3d804f49395639bc9799fc2d949f02138b6134
Eric Friedrich <eric.friedrich84@gmail.com>
2013-03-29 03:04:13 +0000

    Modified Project1-Intermediate.py

--- RaspberryPi_Toolbox/Project1-Intermediate.py
+++ RaspberryPi_Toolbox/Project1-Intermediate.py
@@ -9,6 +9,8 @@
 
 
 import RPi.GPIO
+import time
+import random
 
 ledPin = [14, 15, 18]
 switchPin = [4, 17, 21]
@@ -16,5 +18,15 @@ switchPin = [4, 17, 21]
 
 GPIO.setmode(GPIO.BCM)
 for pin in ledPin:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.LOW)
     
+for pin in switchPin:
+    GPIO.setup(pin, GPIO.IN)
+
+
+print "Starting Simon Says"
+seqLength = 1;
+seq = [random() for i in range(0, seqLength)]
+print "Correct sequence is "+seq
     
