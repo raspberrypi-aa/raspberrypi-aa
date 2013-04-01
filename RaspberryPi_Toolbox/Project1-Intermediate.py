47570de88cf095af36ac47d112e8562869ff4883
Eric Friedrich <eric.friedrich84@gmail.com>
2013-04-01 03:38:14 +0000

    Modified Project1-Intermediate.py

--- RaspberryPi_Toolbox/Project1-Intermediate.py
+++ RaspberryPi_Toolbox/Project1-Intermediate.py
@@ -18,12 +18,13 @@ import random
 ledPin = [14, 15, 18]
 switchPin = [4, 17, 21]
 
+def genSeq(length):
+    '''Generate a sequence (ledPin, switchPin) of given length'''
+    retList = []
+    for x in xrange(0, length):
+        r = r.randomInt(0, len(ledPin))
+        retList.append((ledPin[r], switchPin[r]))
 
-def wait_for_input(pin, state):
-    'Sit in while loop until pin state changes. Sleep to avoid hogging CPU'
-    while GPIO.input(pin) != state:
-        time.sleep(.02)
-        
 def get_next_pin_low(pinList, timeout):
     '''Sit in while loop until any pin in pinList goes low. If no pin pressed
     before timeout, return false. Return number of pin pressed'''
@@ -39,8 +40,9 @@ def get_next_pin_low(pinList, timeout):
 def checkSeq(seq, timeout):
     '''Wait for user to enter the sequence. Return true if correct. Return
     false if wrong pin pressed or timeout reached'''
+    for step in seq:
+        
     
-
 
 def blinkSeq(seq):
     'Blink LEDs in the correct sequence to teach player the pattern.'
@@ -51,18 +53,19 @@ def blinkSeq(seq):
         GPIO.output(pin, GPIO.LOW)
 
 
-GPIO.setmode(GPIO.BCM)
-for pin in ledPin:
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, GPIO.LOW)
+if __name__ == '__main__':
+    GPIO.setmode(GPIO.BCM)
+    for pin in ledPin:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, GPIO.LOW)
+        
+    for pin in switchPin:
+        GPIO.setup(pin, GPIO.IN)
     
-for pin in switchPin:
-    GPIO.setup(pin, GPIO.IN)
-
-print "Starting Simon Says"
-seqLength = 1;
-timeout = 10; # User must complete pattern in 10 seconds or its Game Over
-seq = [random() for i in range(0, seqLength)]
-blinkSeq(seq)
-checkSequence(seq)
+    print "Starting Simon Says"
+    seqLength = 1;
+    timeout = 10; # User must complete pattern in 10 seconds or its Game Over
+    curSeq = genSeq(seqLength)
+    blinkSeq(ledSeq)
+    checkSequence(seq)
     
