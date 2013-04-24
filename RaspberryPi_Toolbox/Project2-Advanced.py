2e9c4be2072a040fec79d0e8bbb61da79a00e7e7
Eric Friedrich <eric.friedrich84@gmail.com>
2013-04-23 22:32:08 -0400

    Modified Project2-Advanced.py

--- RaspberryPi_Toolbox/Project2-Advanced.py
+++ RaspberryPi_Toolbox/Project2-Advanced.py
@@ -2,3 +2,30 @@
 #
 # Project 2 Advanced - Frequency Counter
 # 
+
+
+import RPi.GPIO as GPIO
+import time
+
+switchPin = 4
+updateIntervalSeconds=5
+
+counter = 0
+
+def fallingEdge():
+    global counter += 1    
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(switchPin, GPIO.IN)
+
+GPIO.add_event_detect(switchPin, GPIO.FALLING)
+GPIO.add_event_callback(switchPin, fallingEdge)
+
+while True:
+    time.sleep(updateIntervalSeconds)
+    freq = counter / updateIntervalSeconds
+    counter = 0
+    print "Frequency: %d Hz" % (freq)
+    
+
+
