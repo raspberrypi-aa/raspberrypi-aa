04c0aa2d2cb4828dc0937a0ad6918f09e4443c21
Eric Friedrich <eric.friedrich84@gmail.com>
2013-04-28 21:57:14 -0400

    Modified spi_bitbang_test.py

--- RaspberryPi_Toolbox/spi_bitbang_test.py
+++ RaspberryPi_Toolbox/spi_bitbang_test.py
@@ -10,13 +10,12 @@
 
 import RPi.GPIO as GPIO
 import time
-
+import sys
 
 CLK = 18
 MISO = 23
 MOSI = 24
 CS = 25
-
 
 def setupSpiPins(clkPin, misoPin, mosiPin, csPin):
     ''' Set all pins as an output except MISO (Master Input, Slave Output)'''
@@ -26,7 +25,7 @@ def setupSpiPins(clkPin, misoPin, mosiPin, csPin):
     GPIO.setup(csPin, GPIO.OUT)
      
 
-def readAdc(channel=0, clkPin, misoPin, mosiPin, csPin):
+def readAdc(channel, clkPin, misoPin, mosiPin, csPin):
     if (channel < 0) or (channel > 7):
         print "Invalid ADC Channel number, must be between [0,7]"
         return -1
@@ -47,13 +46,15 @@ def readAdc(channel=0, clkPin, misoPin, mosiPin, csPin):
     read_command = 0x18
     read_command |= channel
     
-    sendBits(read_command, 5)
+    sendBits(read_command, 5, clkPin, mosiPin)
     
     adcValue = recvBits(12, clkPin, misoPin)
     
     # Set chip select high to end the read
     GPIO.output(csPin, GPIO.HIGH)
   
+    return adcValue
+    
 def sendBits(data, numBits, clkPin, mosiPin):
     ''' Sends 1 Byte or less of data'''
     
@@ -63,7 +64,7 @@ def sendBits(data, numBits, clkPin, mosiPin):
         # Set RPi's output bit high or low depending on highest bit of data field
         if data & 0x80:
             GPIO.output(mosiPin, GPIO.HIGH)
-        else 
+        else:
             GPIO.output(mosiPin, GPIO.LOW)
         
         # Advance data to the next bit
@@ -94,10 +95,14 @@ def recvBits(numBits, clkPin, misoPin):
     
     
 if __name__ == '__main__':
-    GPIO.setmode(GPIO.BCM)
-    setupSpiPins(CLK, MISO, MOSI, CS)
+    try:
+        GPIO.setmode(GPIO.BCM)
+        setupSpiPins(CLK, MISO, MOSI, CS)
     
-    while True:
-        val = readAdc(0, CLK, MISO, MOSI, CS)
-        print "ADC Result: ", str(val)
-        time.sleep(5)
+        while True:
+            val = readAdc(0, CLK, MISO, MOSI, CS)
+            print "ADC Result: ", str(val)
+            time.sleep(5)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        sys.exit(0)
