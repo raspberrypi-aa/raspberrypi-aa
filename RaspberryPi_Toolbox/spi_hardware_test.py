57bdf42b27f9237635fed087267c24abcfee243d
Eric Friedrich <eric.friedrich84@gmail.com>
2013-04-29 21:00:06 -0400

    Modified spi_hardware_test.py

--- RaspberryPi_Toolbox/spi_hardware_test.py
+++ RaspberryPi_Toolbox/spi_hardware_test.py
@@ -20,11 +20,20 @@ def buildReadCommand(channel):
     singleEnded = 0x08
     
     return [startBit, singleEnded|(channel<<4), 0]
+    
+def processAdcValue(result):
+    '''Take in result as array of three bytes. 
+       Return the two lowest bits of the 2nd byte and
+       all of the third byte'''
+       byte2 = (r[1] & 0x03)
+      
+       return (byte2 << 8) | r[2]
+    
         
 def readAdc(channel):
     if ((channel > 7) or (channel < 0)):
         return -1
-    r = spi.xfer2(buildReadCommand())
+    r = spi.xfer2(buildReadCommand(channel))
     adcout = ((r[1]&3) << 8) + r[2]
     return adcout    
         
