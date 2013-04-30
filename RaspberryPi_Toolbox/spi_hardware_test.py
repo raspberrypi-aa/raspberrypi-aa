f9a21725a1467c883c5fc3ccc576d7fcb0048c64
Eric Friedrich <eric.friedrich84@gmail.com>
2013-04-29 20:56:17 -0400

    Modified spi_hardware_test.py

--- RaspberryPi_Toolbox/spi_hardware_test.py
+++ RaspberryPi_Toolbox/spi_hardware_test.py
@@ -3,22 +3,28 @@
 # Bitbang'd SPI interface with an MCP3008 ADC device
 # MCP3008 is 8-channel 10-bit analog to digital converter
 #  Connections are:
-#     CLK => 18  
-#     DOUT => 23 (chip's data out, RPi's MISO)
-#     DIN => 24  (chip's data in, RPi's MOSI)
-#     CS => 25 
+#     CLK => SCLK  
+#     DOUT =>  MISO
+#     DIN => MOSI
+#     CS => CE0 
 
 import time
 import sys
 import spidev
 
-s = spidev.SpiDev()
-s.open(0,0)
+spi = spidev.SpiDev()
+spi.open(0,0)
+
+def buildReadCommand(channel):
+    startBit = 0x01
+    singleEnded = 0x08
+    
+    return [startBit, singleEnded|(channel<<4), 0]
         
 def readAdc(channel):
     if ((channel > 7) or (channel < 0)):
         return -1
-    r = spi.xfer2([1,(8+channel)<<4,0])
+    r = spi.xfer2(buildReadCommand())
     adcout = ((r[1]&3) << 8) + r[2]
     return adcout    
         
