5139aeb8ad096e9153cd51876c4473bb399d6fb5
Eric Friedrich <eric.friedrich84@gmail.com>
2013-05-01 21:19:14 -0400

    Modified i2c_mcp23008_test.py

--- RaspberryPi_Toolbox/i2c_mcp23008_test.py
+++ RaspberryPi_Toolbox/i2c_mcp23008_test.py
@@ -4,7 +4,47 @@
 #
 #
 
-mcp = Adafruit_MCP230XX(busnum = 1, address = 0x20, num_gpios = 16)
+import Adafruit_I2C
 
-mcp.config(0, OUTPUT)
-mcp.output(0, 1)
+#if True:
+#    mcp = Adafruit_MCP230XX(busnum = 1, address = 0x20, num_gpios = 16)
+#    mcp.config(0, OUTPUT)
+#    mcp.output(0, 1)
+
+IODIR = 0x00 
+GPIO = 0x09
+GPPU = 0x06
+
+direction = 0
+gpio = 0
+
+i2c = Adafruit_I2C(0x20)
+
+def setAllInput():
+    i2c.write8(IODIR, 0xFF)
+    
+def setAllOutput():
+    i2c.write8(IODIR, 0x00)
+    
+def setPinMode(pin, input):
+    '''Pin is the pin number to modify input/output state of.
+       Input is True to set the pin as input, False to set it as output'''
+    
+    if input:
+        direction |= (1 << pin)
+    else:
+        direction = direction & ~(1<<pin)
+        
+    i2c.write(IODIR, direction)
+
+def setPin(pin, state):
+    if state:
+        gpio = gpio | (1 << pin)
+    else:
+        gpio = gpio & ~(1<<pin)
+        
+
+if __name__ == '__main__'
+    setAllOutput()
+    setPin(1, True)
+    
