83e68719ab78d216952216e659dc1a0d361ff764
Eric Friedrich <eric.friedrich84@gmail.com>
2013-05-08 01:42:49 +0000

    Modified PythonRequests.py

--- RaspberryPi_Toolbox/PythonRequests.py
+++ RaspberryPi_Toolbox/PythonRequests.py
@@ -13,6 +13,6 @@ if True:
 if True:
     searchTerms = {'q' : 'raspberryPi'} 
     r = requests.get('http://google.com/search', params=searchTerms)
-    print r.text
+    print r.raw
     print r.response
 
