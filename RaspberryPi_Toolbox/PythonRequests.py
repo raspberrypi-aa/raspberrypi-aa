65bcde40d34e788902068aeb3fcbf15cae8342f1
Eric Friedrich <eric.friedrich84@gmail.com>
2013-05-08 01:39:51 +0000

    Modified PythonRequests.py

--- RaspberryPi_Toolbox/PythonRequests.py
+++ RaspberryPi_Toolbox/PythonRequests.py
@@ -2,5 +2,12 @@
 
 import requests
 
+# Making a simple request
 r = requests.get('http://google.com')
 print r.text
+
+# Passing parameters to the request
+searchTerms = {'q' : 'raspberryPi'} 
+r = requests.get('http://google.com/search', params=searchTerms)
+print r.text
+
