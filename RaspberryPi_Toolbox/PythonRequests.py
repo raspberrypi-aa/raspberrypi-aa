efc33600f823e78df0c68b1a82005c8ed3dd17bd
Eric Friedrich <eric.friedrich84@gmail.com>
2013-05-09 01:43:19 +0000

    Modified PythonRequests.py

--- RaspberryPi_Toolbox/PythonRequests.py
+++ RaspberryPi_Toolbox/PythonRequests.py
@@ -15,6 +15,14 @@ if True:
     r = requests.get('http://google.com/search', params=searchTerms)
     print r.content
     print r.status_code
+    
+# Setting headers
+if True:
+    searchTerms = {'X-Platform' : 'raspberryPi'} 
+    r = requests.get('http://google.com/search', headers=searchTerms)
+    print r.content
+    print r.status_code
+    
 
 
 #Basic auth
