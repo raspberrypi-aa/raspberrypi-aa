8fe59495713a1c9c6a725e1aee15a490e8a64d3d
Eric Friedrich <eric.friedrich84@gmail.com>
2013-05-09 01:47:49 +0000

    Modified TwitterTest.py

--- RaspberryPi_Toolbox/TwitterTest.py
+++ RaspberryPi_Toolbox/TwitterTest.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 import requests
+import json
 from requests_oauthlib import OAuth1
 
+auth_url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
+post_url = 'https://api.twitter.com/1.1/statuses/update.json'
 
-url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
 auth = OAuth1('YOUR_APP_KEY', 'YOUR_APP_SECRET', 
     'USER_OAUTH_TOKEN', 'USER_OAUTH_TOKEN_SECRET')
 
-requests.get(url, auth=auth)
+requests.get(auth_url, auth=auth)
+
+
