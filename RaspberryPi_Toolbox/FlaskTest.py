2309cc23557336b56bb7737ddb0ab5ad067aaae1
Eric Friedrich <eric.friedrich84@gmail.com>
2013-05-10 00:46:25 +0000

    Modified FlaskTest.py

--- RaspberryPi_Toolbox/FlaskTest.py
+++ RaspberryPi_Toolbox/FlaskTest.py
@@ -13,7 +13,10 @@ def root():
 # Test with: curl http://localhost:5000/echoParam?k=v\&k1=v1
 @app.route('/echoParam')
 def echoParam():
-    return str(request.args['q'])
+    try:
+        return str(request.args['q'])
+    except KeyError:
+        return "Missing parameter"
     
 # Template tesT:
 from flask import render_template
