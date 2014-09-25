026e208f7536db60d1f9687c6258ee2016a5adb0
Friedrich Eric <eric.friedrich84@gmail.com>
2014-09-24 20:11:43 -0400

    Updated servoblaster

--- servoblaster.py
+++ servoblaster.py
@@ -10,18 +10,29 @@ import time
 
 # From /dev/servoblaster.cfg:
 # Servo Channel 0 => GPIO 4
-servoChannel = 0
+servoChannel = 2
 
 def setServo(servoChannel, position):
+    
     servoStr ="%u=%u\n" % (servoChannel, position)
+
+    
     with open("/dev/servoblaster", "wb") as f:
         f.write(servoStr)
+                
     
 
 if __name__ == '__main__':
+    val = 50
+    direction = 1
     while True:
-        for i in range(80, 250):
-            setServo(servoChannel, i)
-            time.sleep(.1)
-        
+        #print val
+        setServo(servoChannel, val)
+        time.sleep(.01)
+    
+        if val == 249:
+            direction -= 1
+        elif val == 50:
+            direction = 1
 
+        val += direction
