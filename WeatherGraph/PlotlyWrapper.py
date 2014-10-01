7ce2859d93f62834e61b63c111ae9863cebb690b
Eric Friedrich <eric.friedrich84@gmail.com>
2014-10-01 01:57:27 +0000

    Modified PlotlyWrapper.py

--- WeatherGraph/PlotlyWrapper.py
+++ WeatherGraph/PlotlyWrapper.py
@@ -18,6 +18,8 @@ class TempPresPlotlyWrapper:
     '''    
 
     def __init__(self):
+        print "Starting up Plot.ly, please wait"
+        
         with open('./config.json') as config_file:
             plotly_user_config = json.load(config_file)
             pltly.sign_in(plotly_user_config["plotly_username"], plotly_user_config["plotly_api_key"])
@@ -52,6 +54,8 @@ class TempPresPlotlyWrapper:
         
         self.presStream = pltly.Stream(plotly_user_config["plotly_streaming_tokens"][1])
         self.presStream.open()
+        
+        print "--Ready to go!"
                         
                             
         
