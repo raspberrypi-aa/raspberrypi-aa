634d632ff518123cbb090a2084c27ec46899548a
Eric Friedrich <eric.friedrich84@gmail.com>
2014-10-01 00:48:45 +0000

    Modified PlotlyWrapper.py

---
+++ WeatherGraph/PlotlyWrapper.py
@@ -0,0 +1,13 @@
+import plotly.plotly as pltly
+
+class TempPresPlotlyWrapper:
+''' This class creates a scatter plot with 2 y-axis data sets
+#
+# Requires 
+# X-Axis: 
+#  - Time
+#
+# Y-Axis: 
+# - Temperature
+# - Pressue
+#    
