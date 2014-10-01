10e2ec587b7a87bbbe701b161ca4b3c492f863f6
Eric Friedrich <eric.friedrich84@gmail.com>
2014-10-01 01:16:42 +0000

    Modified WeatherGraph.py

--- WeatherGraph/WeatherGraph.py
+++ WeatherGraph/WeatherGraph.py
@@ -7,11 +7,17 @@
 
 import Adafruit_BMP.BMP085 as BMP085
 import PlotlyWrapper
+import time
 
 sensor = BMP085.BMP085()
 plotter = PlotlyWrapper.TempPresPlotlyWrapper()
 
-print 'Temp = {0:0.2f} *C'.format(sensor.read_temperature())
-print 'Pressure = {0:0.2f} Pa'.format(sensor.read_pressure())
 print 'Altitude = {0:0.2f} m'.format(sensor.read_altitude())
 print 'Sealevel Pressure = {0:0.2f} Pa'.format(sensor.read_sealevel_pressure())
+
+
+for i in range(0, 10):
+    print 'Temp = {0:0.2f} *C'.format(sensor.read_temperature())
+    print 'Pressure = {0:0.2f} Pa'.format(sensor.read_pressure())
+    plotter.
+    
