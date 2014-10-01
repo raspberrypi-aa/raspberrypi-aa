2a760d368154b39784191d9bdeea878b008848e8
Eric Friedrich <eric.friedrich84@gmail.com>
2014-10-01 00:45:23 +0000

    Modified WeatherGraph.py

---
+++ WeatherGraph/WeatherGraph.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#
+# Reads temperature and barometric pressure from BMP180 SPI sensor
+# Posts results to http://plot.ly
+#
+
+
+import Adafruit_BMP.BMP085 as BMP085
+
+sensor = BMP085.BMP085()
+
+
+print 'Temp = {0:0.2f} *C'.format(sensor.read_temperature())
+print 'Pressure = {0:0.2f} Pa'.format(sensor.read_pressure())
+print 'Altitude = {0:0.2f} m'.format(sensor.read_altitude())
+print 'Sealevel Pressure = {0:0.2f} Pa'.format(sensor.read_sealevel_pressure())
