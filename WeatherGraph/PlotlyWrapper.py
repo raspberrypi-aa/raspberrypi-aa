1a7e2394c41a0ddd94c61fca16618851d82884ef
Eric Friedrich <eric.friedrich84@gmail.com>
2014-10-01 00:53:45 +0000

    Modified PlotlyWrapper.py

--- WeatherGraph/PlotlyWrapper.py
+++ WeatherGraph/PlotlyWrapper.py
@@ -1,4 +1,6 @@
 import plotly.plotly as pltly
+import json
+import datetime
 
 class TempPresPlotlyWrapper:
 ''' This class creates a scatter plot with 2 y-axis data sets
@@ -12,3 +14,22 @@ X-Axis:
  - Temperature
  - Pressue
 '''    
+    def __init__(self):
+        with open('./config.json') as config_file:
+            plotly_user_config = json.load(config_file)
+            pltly.sign_in(plotly_user_config["plotly_username"], plotly_user_config["plotly_api_key"])
+
+        url = pltly.plot([{
+            'x': [],
+            'y': [],
+            'type': 'scatter',
+            'stream': {
+                'token': plotly_user_config['plotly_streaming_tokens'][0], 
+                'maxpoints': 200
+            }
+        }], filename='Raspberry Pi Streaming Example Values')
+
+stream = pltly.Stream(plotly_user_config['plotly_streaming_tokens'][0])
+stream.open()
+
+import time
