4f9ca61a3e7002f5ce477214b86a95b3c41be084
Eric Friedrich <eric.friedrich84@gmail.com>
2015-05-20 01:20:56 +0000

    Modified WeatherStation.py

--- WeatherStation/WeatherStation.py
+++ WeatherStation/WeatherStation.py
@@ -59,7 +59,7 @@ def forecast():
         print "Error:", r.status_code
     print json.dumps(r.json(),
        sort_keys = True,
-       indent=4)
+       indent=4).keys()
     forecast =  r.json()['currently']['icon']
     return render_template('index.html',
                            forecastPage = True,
